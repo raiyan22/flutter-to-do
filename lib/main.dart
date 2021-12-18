e7d7d1334a02214360c6a5f38240f7b653439b67
raiyan22 <raiyanislam058@gmail.com>
2021-12-17 23:18:01 -0800

    added Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp( MaterialApp(
+  debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
+        primaryColor: Colors.blue,
       ),
+  home: MyApp(),
   )
 );
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("To Do List"),
+        ),
+
+    );
+  }
+}
+
+
 
 
